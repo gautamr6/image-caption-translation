1f91ad658057e09145904306de767ba29e441c6c
Gautam Ramesh <gautamshivramesh@gmail.com>
2020-12-18 03:04:02 -0500

    notebook to get stuff for poster

--- data_stored.py
+++ data_stored.py
@@ -1,8 +1,9 @@
 from index_map import indexes
 from get_objects import get_objects
 
-def get_stored_data():
+def get_stored_data(full=False):
     englishFile = open("en-es/source.final.file-found")
+    spanishFile = open("en-es/reference.final.file-found")
     metadataFile = open("en-es/metadata.final.file-found")
 
     azureFile = open("machine-translations/azure.txt")
@@ -15,6 +16,10 @@ def get_stored_data():
     translationData = []
     objectData = []
 
+    if full:
+        sourceData = []
+        imageNameData = []
+
     for i in range(27367):
         # Machine translations
         azureLine = azureFile.readline()
@@ -24,10 +29,21 @@ def get_stored_data():
         # English reference caption
         referenceData.append(englishFile.readline())
 
-        # Objects
+        # Spanish source caption
+        if full:
+            sourceData.append(spanishFile.readline())
+
+        # Image name
         metadataLine = metadataFile.readline()
+        if full:
+            imageNameData.append(metadataLine[:-1])
+
+        # Objects
         imageIndex = indexMap[metadataLine[:-1]]
         imageNumber = int(imageIndex[3:]) - 1
         objectData.append(objects[imageNumber])
     
-    return (referenceData, translationData, objectData)
+    if full:
+        return (referenceData, translationData, objectData, imageNameData, sourceData)
+    else:
+        return (referenceData, translationData, objectData)
